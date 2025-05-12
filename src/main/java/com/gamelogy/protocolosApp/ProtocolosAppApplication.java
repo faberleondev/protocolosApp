7ff0ede475970d6c3dfbfb9e21e8ab6b0491afb3
faberleondev <fabricio.sebastian.leon@outlook.com>
2025-05-12 19:48:32 -0300

    First Release

---
+++ src/main/java/com/gamelogy/protocolosApp/ProtocolosAppApplication.java
@@ -0,0 +1,13 @@
+package com.gamelogy.protocolosApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocolosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocolosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamelogy/protocolosApp/ProtocolosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamelogy.protocolosApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtocolosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
